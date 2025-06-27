6fdab3996ffe45ed57e83847cbb0888e13ddb04a
jmoo2880 <joshua.moore@sydney.edu.au>
2025-06-27 17:51:52 +1000

    modify the calculator to handle dynamic imports

--- pyhctsa/FeatureCalculator/calculator.py
+++ pyhctsa/FeatureCalculator/calculator.py
@@ -1,63 +1,67 @@
 import yaml
 from functools import partial
 import importlib
-from ..Operations import Medical
+from numpy.typing import ArrayLike
 from itertools import product
 from ..Utilities.utils import preprocess_decorator
 
 class FeatureCalculator:
-    def __init__(self, config_path):
-        with open(config_path) as f:
+    def __init__(self, configPath, verbose=False):
+        with open(configPath) as f:
             self.config = yaml.safe_load(f)
+        self.operations_package = "pyhctsa.Operations" # abs path
         self.feature_funcs = self._build_feature_funcs()
+        self.verbose = verbose # whether to print summary when loading
 
     def _build_feature_funcs(self):
         feature_funcs = {}
-        for feature_name, feature_config in self.config["Medical"].items():
-            op_func = getattr(Medical, feature_name, None)
-            print(op_func)
-            if op_func is None:
-                continue
-            configs = feature_config.get("configs", [{}])
-            if isinstance(configs, list) and configs and isinstance(configs[0], dict):
-                for conf in configs:
-                   # extract the preprocessing flags
-                    zscore = conf.pop("zscore", False) if "zscore" in conf else False
-                    absval = conf.pop("abs", False) if "abs" in conf else False
-                    label = f"{feature_name}"
+        for module_key in self.config.keys():
+            # Dynamically import the module based on the config key
+            module = importlib.import_module(f"{self.operations_package}.{module_key}")
+            for feature_name, feature_config in self.config[module_key].items():
+                op_func = getattr(module, feature_name, None)
+                if op_func is None:
+                    continue
+                configs = feature_config.get("configs", [{}])
+                if isinstance(configs, list) and configs and isinstance(configs[0], dict):
+                    for conf in configs:
+                        zscore = conf.pop("zscore", False) if "zscore" in conf else False
+                        absval = conf.pop("abs", False) if "abs" in conf else False
+                        label = f"{module_key}_{feature_name}"
+                        if zscore:
+                            label += "_zscoreTrue"
+                        if absval:
+                            label += "_absTrue"
+                        if conf:
+                            keys, values = zip(*[(k, v if isinstance(v, list) else [v]) for k, v in conf.items()])
+                            for combo in product(*values):
+                                combo_dict = dict(zip(keys, combo))
+                                label_full = label
+                                if combo_dict:
+                                    label_full += "_" + "_".join(f"{k}{v}" for k, v in combo_dict.items())
+                                decorated_func = preprocess_decorator(zscore, absval)(op_func)
+                                feature_funcs[label_full] = partial(decorated_func, **combo_dict)
+                        else:
+                            decorated_func = preprocess_decorator(zscore, absval)(op_func)
+                            feature_funcs[label] = decorated_func
+                else:
+                    zscore, absval = False, False
+                    if isinstance(configs, list) and configs and isinstance(configs[0], dict):
+                        zscore = configs[0].pop("zscore", False)
+                        absval = configs[0].pop("abs", False)
+                    label = f"{module_key}_{feature_name}"
                     if zscore:
                         label += "_zscoreTrue"
                     if absval:
                         label += "_absTrue"
-                    # Handle empty conf (no additional params)
-                    if conf:
-                        keys, values = zip(*[(k, v if isinstance(v, list) else [v]) for k, v in conf.items()])
-                        for combo in product(*values):
-                            combo_dict = dict(zip(keys, combo))
-                            label_full = label
-                            if combo_dict:
-                                label_full += "_" + "_".join(f"{k}{v}" for k, v in combo_dict.items())
-                            decorated_func = preprocess_decorator(zscore, absval)(op_func)
-                            feature_funcs[label_full] = partial(decorated_func, **combo_dict)
-                    else:
-                        decorated_func = preprocess_decorator(zscore, absval)(op_func)
-                        feature_funcs[label] = decorated_func
-            else:
-                # no configs i.e., no additional function params
-                zscore, absval = False, False
-                if isinstance(configs, list) and configs and isinstance(configs[0], dict):
-                    zscore = configs[0].pop("zscore", False)
-                    absval = configs[0].pop("abs", False)
-                label = f"{feature_name}"
-                if zscore:
-                    label += "_zscoreTrue"
-                if absval:
-                    label += "_absTrue"
-                decorated_func = preprocess_decorator(zscore, absval)(op_func)
-                feature_funcs[label] = decorated_func
+                    decorated_func = preprocess_decorator(zscore, absval)(op_func)
+                    feature_funcs[label] = decorated_func
         return feature_funcs
-    
-    def extract(self, data):
+
+    def extract(self, data : ArrayLike) -> dict:
+        """
+        Extract all time-series features.
+        """
         results = {}
         for name, func in self.feature_funcs.items():
             try:
