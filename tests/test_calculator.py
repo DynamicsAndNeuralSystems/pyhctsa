9653f1d7a9b40cef1cd622013bc11267c9070379
jmoo2880 <joshua.moore@sydney.edu.au>
2025-07-08 12:00:46 +1000

    wavelet module unit tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,7 +8,7 @@ import pytest
 # does the calculator run on module functions (yes/no)?
 @pytest.mark.parametrize("x", [
     "medical", "extreme", "criticality", "correlation", "information", "entropy",
-    "stationarity", "distribution", "scaling", "symbolic"])
+    "stationarity", "distribution", "scaling", "symbolic", "wavelet"])
 def test_module_basic(x):
     # basic checks on medical module
     data = get_dataset(which="sinusoid")
